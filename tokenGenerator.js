7928a2c30543d683ea06e2aa83dbd41c840edf0e
Rudolf <spureszka@gmail.com>
2022-09-23 10:16:33 +0200

    generate method removed

--- tokenGenerator.js
+++ tokenGenerator.js
@@ -4,31 +4,6 @@ const saltingMyToken = require("./generator/async/withMyOwnCharacters");
 
 class Generator {
   /**
-   * @description generate() with two parameters
-   *
-   * @param {string} type "normal", "medium", "extra" or "onlyNumbers"
-   *
-   * - "normal" = (a-z)
-   * - "medium" = (a-z + 0-9)
-   * - "extra" = (a-Z + 0-9)
-   * - "onlyNumbers" = (0-9)
-   *
-   * @param {number} rounds a number, this tells you the length of the token
-   * @example generate("extra", 100)
-   * @returns {string} a string
-   */
-  generate(type, rounds) {
-    console.log(
-      "--> After v1.9.0 generate method will not be available. Use genSync() instead <--"
-    );
-    const [isValid, msg] = this.#validParameters(type, rounds);
-    if (!isValid) {
-      console.log(msg);
-    } else {
-      return normalToken(type, rounds);
-    }
-  }
-  /**
    * @description like generate function but returns with Promise
    *
    * @param {string} type "normal", "medium", "extra" or "onlyNumbers"
