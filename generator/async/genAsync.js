08f0d776c1f015fe68335ae0a8c25ef4c5495d85
Rudolf <spureszka@gmail.com>
2022-09-24 22:48:52 +0200

    rounds renamed to length in genAsync

--- generator/async/genAsync.js
+++ generator/async/genAsync.js
@@ -1,6 +1,6 @@
 const { normal, medium, extra } = require("../token/types");
 
-module.exports = (type, rounds) => {
+module.exports = (type, length) => {
   return new Promise((resolve, reject) => {
     let newToken = [];
     let info = {};
@@ -14,7 +14,7 @@ module.exports = (type, rounds) => {
       info.usedToken = extra;
       info.length = extra.length;
     } else if (type === "onlyNumbers") {
-      for (let i = 0; i < rounds; i++) {
+      for (let i = 0; i < length; i++) {
         const randomNumber = Math.floor(Math.random() * 10); //Generate 0-9 random number
         if (i === 0 && randomNumber === 0) {
           newToken.push("1");
@@ -22,16 +22,16 @@ module.exports = (type, rounds) => {
           newToken.push(randomNumber);
         }
       }
-      if (newToken.length === rounds) {
+      if (newToken.length === length) {
         return resolve(newToken.join(""));
       }
     }
-    for (let i = 0; i < rounds; i++) {
+    for (let i = 0; i < length; i++) {
       const randomNumber = Math.floor(Math.random() * info.length);
       const tokenChararacter = info.usedToken[randomNumber];
       newToken.push(tokenChararacter);
     }
-    if (newToken.length === rounds) {
+    if (newToken.length === length) {
       return resolve(newToken.join(""));
     }
   });
