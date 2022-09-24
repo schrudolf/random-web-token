81c58bda7b6884898d2f72b25161d1ae43178697
Rudolf <spureszka@gmail.com>
2022-09-24 22:49:33 +0200

    rounds renamed to length in withMyOwnCharacters

--- generator/async/withMyOwnCharacters.js
+++ generator/async/withMyOwnCharacters.js
@@ -1,12 +1,12 @@
-module.exports = (types, rounds) => {
+module.exports = (types, length) => {
     return new Promise((resolve, reject) => {
         let newToken = [];
-        for (let i = 0; i < rounds; i++) {
+        for (let i = 0; i < length; i++) {
             const randomNumber = Math.floor(Math.random() * types.length);
             const tokenChararacter = types[randomNumber];
             newToken.push(tokenChararacter);
         }
-        if (newToken.length === rounds) {
+        if (newToken.length === length) {
             return resolve(newToken.join(""));
         }
     })
