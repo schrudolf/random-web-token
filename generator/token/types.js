84bc1252e4ab7d53c67fba581e9ea257c1c2fe48
Rudolf <spureszka@gmail.com>
2022-09-28 11:28:01 +0200

    new number template added

--- generator/token/types.js
+++ generator/token/types.js
@@ -1,9 +1,11 @@
 const normal = "abcdefghijklmnopqrstuvwxyz";
 const medium = "abcdefghijklmnopqrstuvwxyz0123456789";
 const extra = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const onlyNumbers = "0123456789";
 
 module.exports = {
   normal,
   medium,
   extra,
+  onlyNumbers
 };
