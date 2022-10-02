c44184dfd9657c21be508f8c4d739c0fd8332593
Rudolf <spureszka@gmail.com>
2022-10-02 20:09:06 +0200

    getTypeTemplate moved to an external file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const getTypeTemplate = require("./lib/tokenTemplates/getTypeTemplate");
 const getSyncToken = require("./lib/sync/genSync");
 const getAsyncToken = require("./lib/async/genAsync");
 const getTokenWithMyOwnCharacters = require("./lib/async/withMyOwnCharacters");
@@ -13,8 +14,6 @@ const availableTypes = [
   "extra",
   "onlyNumbers",
 ];
-const letters = "abcdefghijklmnopqrstuvwxyz";
-const numbers = "0123456789";
 
 const generator = {
   /**
@@ -143,25 +142,6 @@ const generator = {
     }
   },
 };
-
-function getTypeTemplate(type) {
-  if (type === "normal") {
-    return letters;
-  } else if (type === "normal+") {
-    return letters.toUpperCase();
-  } else if (type === "medium") {
-    let medium = letters + numbers;
-    return medium;
-  } else if (type === "medium+") {
-    let mediumPlus = letters.toUpperCase() + numbers;
-    return mediumPlus;
-  } else if (type === "extra") {
-    let extra = letters + letters.toUpperCase() + numbers;
-    return extra;
-  } else {
-    return numbers;
-  }
-}
 
 function checkValidatorParameters(token, allowedPlusCharacters) {
   try {
